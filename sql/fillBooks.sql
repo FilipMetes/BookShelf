d0c736cfb4d6b2e016a67ca14220d8904df84d6a
M3T3S4K <filipmetes04@gmail.com>
2025-12-06 17:44:08 +0100

    profile edit + book search demo

---
+++ sql/fillBooks.sql
@@ -0,0 +1,14 @@
+INSERT INTO books
+(id, title, author, genre, format, year, price, number_availible, pages, text, sample_path, cover_path)
+VALUES
+    (1, 'Tales of Horror', 'H.P.Lovecraft', 'Cosmic Horror', 'F', 0, 15, 0, 0, '', NULL, '1765020691-81DQAJxsBnL._AC_UF894,1000_QL80_.jpg');
+
+INSERT INTO books
+(id, title, author, genre, format, year, price, number_availible, pages, text, sample_path, cover_path)
+VALUES
+    (7, 'Zaklínač', 'Andrej Sapkowski', 'akčné', 'F', 0, 25, 35, 700, '', NULL, '1765035079-0023146419_cover_zaklinac_1 1400px.jpg');
+
+INSERT INTO books
+(id, title, author, genre, format, year, price, number_availible, pages, text, sample_path, cover_path)
+VALUES
+    (9, 'Harry Potter and the Philosophers Stone', 'J. K. Rowling', 'Fantasy', 'F', 0, 20, 15, 223, 'The first book of the Harry Potter series.', NULL, '1765035086-81q77Q39nEL._AC_UF10001000_QL80_DpWeblab_.jpg');
---
+++ sql/fillUsers.sql
@@ -0,0 +1,9 @@
+INSERT INTO users
+(id, name, surname, city, PSC, street, e_mail, password, role)
+VALUES
+    (1, 'Jakub', 'Jasak', 'Rabca', '02944', 'Adamovka', 'jakubjasak@gmail.com', '$2y$10$TxIKCUYY2w9DR22gGllIW.7sAACRdaeEWoLn.vT1Gqxtdir/oxAYu', 'U');
+
+INSERT INTO users
+(id, name, surname, city, PSC, street, e_mail, password, role)
+VALUES
+    (2, 'Filip', 'Metes', 'Oravská Polhora', '02947', 'Lipová', 'filipmetes04@gmail.com', '$2y$10$UFY.Um1d670441H0u9BmROU8opxl43mqWmSmmhwEjZcP.w8C30hca', 'A');
